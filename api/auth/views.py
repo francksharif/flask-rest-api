7b01502fd39b01c5cab2d3e369ceba53f6166a9a
francksharif <badinisharif23@gmail.com>
2024-07-30 17:54:43 -0400

    creating user informations access when logged in and token refresh view

--- api/auth/views.py
+++ api/auth/views.py
@@ -4,9 +4,12 @@ from . import auth
 from werkzeug.security import check_password_hash, generate_password_hash
 from ..models import User
 from ..constants.http_status_code import *
+from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, jwt_required
 from .. import db
 
 
+
+# Registering a new User 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     username = request.json['username']
@@ -48,3 +51,58 @@ def register():
             'username': username, 'email': email
         }
     }), HTTP_201_CREATED
+
+
+
+# Login a registered User
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    user  = User.query.filter_by(email=email).first()
+    
+    if user:
+        if check_password_hash(user.password, password):
+            refresh = create_refresh_token(identity=user.id)
+            access = create_access_token(identity=user.id)
+
+
+            return jsonify({
+                'message': 'User logged in successfully.',
+                'user':{
+                    'refresh': refresh,
+                    'access': access,
+                    'username': user.username,
+                    'email': user.email
+                }
+            }), HTTP_200_OK
+
+
+    return jsonify({
+            'error': 'Wrong credentials. Please try again.'
+        }), HTTP_401_UNAUTHORIZED
+
+
+# Get User information when logged in
+@auth.route('/me')
+@jwt_required()
+def me():
+    user_id = get_jwt_identity()
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        'username': user.username,
+        'email': user.email
+    }), HTTP_200_OK
+
+
+# Refresh User token
+@auth.route('/token/refresh')
+@jwt_required(refresh=True)
+def refresh_users_token():
+    identity = get_jwt_identity()
+    access = create_access_token(identity=identity)
+
+    return jsonify({
+        'access': access,
+    }), HTTP_200_OK
--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
